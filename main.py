646d566a6dd30f82eae8240d964051b983b11fa9
Eivind Teig <eivind.teig@gmail.com>
2018-04-02 00:05:25 +0200

    UHS88a decoder, header next

--- main.py
+++ main.py
@@ -9,4 +9,52 @@ if i<80
 else
   return 2*i-127
 endif
+
+File composition
+
+Line  Content
+1    "UHS", a magic marker indicating a UHS file.
+2    "Borrowed Time", the main title
+3    "73" the line number of the first hint (meaning line 77)
+4    "143" the line number of the last hint (meaning line 147)
+[end of header]
+
+[directory tree]
+5    "gHrGtGs iqrGr"="Opening Scene"
+6    "23" the line number jumped to when selecting this link (meaning line 27)
+[...]
+25   "kIpHHtGs jH JDr apyr"="Wrapping Up the Case"
+26   "69" -- this is the last entry of the main directory, since a link above linked to line 27.
+27   "kDpJ Bw d Bw tG JDr wCCtqr"="What do I do in the office" (note: no question mark)
+28   "73" link destination line no (meaning line 77)
+29   "4w{ Bw d ryqpHr CIwv JDr JDzsy"="How do I escape from the thugs" (note: no question mark)
+30   "76" link destination line no (meaning line 80)
+[...]
+75   "4w{ Bw d pIIryJ 3pIGDpv"="How do I arrest Farnham" (note: no question mark)
+76   "141" link destination line no (meaning line 145)
+[end of directory tree]
+
+[hints]
+77   This is the first hint line, as indicated in the header.
+[...]
+147  This is the last hint line, as indicated in the header.
 """
+def read_file(filename: str) -> list:
+    with open(filename) as f: lines = [line.rstrip('\n') for line in f]
+    return lines
+
+
+test_string = "|wz pIr zytGs Bwry GwJ yzHHwIJ'  ;tytJ JDr j4i {rA ytJr pJ"
+def decode_UHS88a(mystr: str) -> str:
+    result = []
+    for i in mystr:
+        code = ord(i)
+        if code<32:
+            result.append(chr(code))
+        elif code<80:
+            result.append(chr(2*code-32))
+        else:
+            result.append(chr(2*code-127))
+    return ''.join(result)
+
+
